d66c592d40b35721727c411703fefb4e496f5cf4
srikanthSMSCountry <srikanth.korada@smscountry.com>
2019-11-07 14:55:11 +0530

    commiting

--- src/test/java/jenkins/NewTest.java
+++ src/test/java/jenkins/NewTest.java
@@ -8,9 +8,10 @@ public class NewTest {
 
 	public static WebDriver driver;
 	public static String userDirectory;
-
+	
 	@Test
 	public void fun() throws InterruptedException {
+		userDirectory = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", userDirectory + "\\Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
